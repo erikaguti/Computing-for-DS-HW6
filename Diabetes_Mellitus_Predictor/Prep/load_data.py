c92ce8376c6ecfabf7041b3b5410155cb5f38373
Erika Gutierrez <erikagutierrez@Erikas-MacBook-Pro.local>
2022-11-23 23:35:51 +0100

    started making class structures

---
+++ Diabetes_Mellitus_Predictor/Prep/load_data.py
@@ -0,0 +1,11 @@
+class Intake:
+    def __init__(self, data):
+        self.dataset = data
+    def train(self):
+        return
+    def test(self):
+        return
+
+
+
+
---
+++ Diabetes_Mellitus_Predictor/Prep/preprocessor.py
@@ -0,0 +1,7 @@
+class Preprocess:
+    def __init__(self, data):
+        self.dataset = data
+    def removenulls(self):
+        return 
+    def fillnulls(self):
+        return
