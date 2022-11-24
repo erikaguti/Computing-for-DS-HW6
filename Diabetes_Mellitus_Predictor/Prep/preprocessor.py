00d093bcc1fc92bbee8c69f187de5e8911effcf3
Erika Gutierrez <erikagutierrez@Erikas-MacBook-Pro.local>
2022-11-24 12:45:53 +0100

    initial draft of the Preprocess Classes

--- Diabetes_Mellitus_Predictor/Prep/preprocessor.py
+++ Diabetes_Mellitus_Predictor/Prep/preprocessor.py
@@ -1,7 +1,13 @@
-class Preprocess:
-    def __init__(self, data):
-        self.dataset = data
-    def removenulls(self):
-        return 
-    def fillnulls(self):
-        return
+import numpy as np
+
+class PreprocessRemove:
+    def removenullrows(self, df, columns):
+        for col in columns:
+            df.dropna(col, inplace = True)
+        return df
+
+class PreprocessFill:
+    def fillnulls(self, df, columns):
+        for col in columns:
+            df[col].fillna(df[col].mean(), inplace = True)
+        return df
