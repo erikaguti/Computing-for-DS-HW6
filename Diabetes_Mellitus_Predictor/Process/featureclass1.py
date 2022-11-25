0e32c675f7b89bcda1a42e143cb5b031318be1a2
Erika Gutierrez <erikagutierrez@Erikas-MacBook-Pro.local>
2022-11-25 15:50:18 +0100

    second draft of feature children classes

--- Diabetes_Mellitus_Predictor/Process/featureclass1.py
+++ Diabetes_Mellitus_Predictor/Process/featureclass1.py
@@ -2,13 +2,11 @@ from featureparent import Feature
 import pandas as pd
 
 class Dummy(Feature):
-    def transform(df, column):
+    def transform(self, df, column):
         return pd.get_dummies(df[column])
 
-    def apply(transformeddf, originaldf):
+    def apply(self, transformeddf, originaldf):
         for col in transformeddf.columns:
             originaldf[f'{col}'] = transformeddf[f'{col}']
         return originaldf
         
-
-
--- Diabetes_Mellitus_Predictor/Process/featureclass2.py
+++ Diabetes_Mellitus_Predictor/Process/featureclass2.py
@@ -2,5 +2,26 @@ from featureparent import Feature
 import pandas as pd
 
 
-data = pd.read_csv('sample_diabetes_mellitus_data.csv')
+class Interaction(Feature):
+    def transform(self, df, column):
+        interactions = {}
+        for col in df.columns[1:-1]:
+            if column != col:
+                if df[col].dtype == df[column].dtype:
+                    try:
+                        interactions.update({column+"_x_"+col: list(df[column]*df[col])})
+                    except TypeError:
+                        interactions.update({column+"_x_"+col: list(df[column]+df[col])})
+        return pd.DataFrame.from_dict(interactions)
+
+    def apply(self, transformeddf, originaldf):
+        for col in transformeddf.columns:
+            originaldf[f'{col}'] = transformeddf[f'{col}']
+        return originaldf
+
+
+
+
+
+
 
